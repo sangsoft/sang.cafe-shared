7e2ff8e48c16d70d0c2a8b38457826a79f53ad62
Trung Nguyen <nquangtrung@gmail.com>
2020-08-24 10:53:59 +0700

    Fix item number

--- dist/constants.d.ts
+++ dist/constants.d.ts
@@ -1,5 +1,5 @@
 export declare const ITEM_PER_PAGE = 10;
-export declare const ITEM_PER_PAGE_FULL = 10;
+export declare const ITEM_PER_PAGE_FULL = 24;
 export declare const RIGHT_COL_SPONSOR_LIMIT = 2;
 export declare const BILL_SHORT_CODE_SIZE = 6;
 export declare const OUR_REGION = "asia-east2";
