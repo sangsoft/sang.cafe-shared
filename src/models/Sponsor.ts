924a6de4d3ef26109775b5b2106d820598aa00d1
nquangtrung <nquangtrung@gmail.com>
2020-12-17 17:16:10 +0700

    Add ISponsor

---
+++ src/models/Sponsor.ts
@@ -0,0 +1,10 @@
+export interface ISponsor {
+  billId: string;
+  createdAt: any;
+  expiredAt: any;
+  level: number;
+  pickCount: number;
+  planId: string;
+  restaurantId: string;
+  disabled: boolean;
+}
