c2112035263bf0a97fab706d85588c417641d2c2
nquangtrung <nquangtrung@gmail.com>
2021-07-20 11:28:33 +0700

    Add user to search match

--- dist/models/SearchMatch.d.ts
+++ dist/models/SearchMatch.d.ts
@@ -1,9 +1,11 @@
 import { SearchRecord } from "./SearchRecord";
 import * as admin from 'firebase-admin';
+import { IUser } from "./User";
 export interface SearchMatch {
     search: SearchRecord;
     fields: string[];
     fieldMatchingCount: number;
     createdAt: admin.firestore.Timestamp;
     score: number;
+    user?: IUser;
 }
--- src/models/SearchMatch.ts
+++ src/models/SearchMatch.ts
@@ -1,6 +1,7 @@
 import { SearchRecord } from "./SearchRecord";
 
 import * as admin from 'firebase-admin';
+import { IUser } from "./User";
 
 export interface SearchMatch {
     search: SearchRecord,
@@ -8,5 +9,6 @@ export interface SearchMatch {
     fieldMatchingCount: number,
     createdAt: admin.firestore.Timestamp,
     score: number
+    user?: IUser;
   }
   
