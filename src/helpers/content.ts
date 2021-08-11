bfecbbdec871e36bc38efeaf2fedc72523a96aa2
Tung Dam <damtung2000@gmail.com>
2021-08-11 19:45:55 +0900

    cleanPhoneNumber function working

--- src/helpers/content.ts
+++ src/helpers/content.ts
@@ -1,3 +1,14 @@
+const pattern = /(?:\+?([\d|o|O]{1,3}))?[-. (]*([\d|o|O]{3})[-. )]*([\d|o|O]{3,4})[-. ]*([\d|o|O]{4})/; //matches most phone numbers w/wo hyphen, dots, spaces, country code
+const advancedPattern = /(?:\+?([\d|o|O]{1,3}))?[-. (]*([\d|o|O]+\w{2,}([\d|o|O]+)|\w+){10,}/; //check for tricks like 038tam88888
+
 export function cleanPhoneNumber(text: string): string {
-  return text;
+  var output: string;
+  if (pattern.test(text) || advancedPattern.test(text)) {
+    const matches = pattern.exec(text) || advancedPattern.exec(text);
+    output = text.replace(matches[0].trim(), '{{phone_number}}');
+    console.log(matches[0].trim())
+    return output;
+  } else {
+    return text;
+  }
 }
