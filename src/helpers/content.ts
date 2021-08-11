39904c9bfa338ba839676768d207857d95f080e3
Tung Dam <damtung2000@gmail.com>
2021-08-11 22:53:40 +0900

    modified cleanPhoneNumber function to accept an array of patterns

--- src/helpers/content.ts
+++ src/helpers/content.ts
@@ -1,8 +1,22 @@
 const pattern = /(?:\+?([\d|o|O]{1,3}))?[-. (]*([\d|o|O]{3})[-. )]*([\d|o|O]{3,4})[-. ]*([\d|o|O]{4})/; //matches most phone numbers w/wo hyphen, dots, spaces, country code
-const advancedPattern = /(?:\+?([\d|o|O]{1,3}))?[-. (]*([\d|o|O]+\w{2,}([\d|o|O]+)|\w+){10,}/; //check for tricks like 038tam88888
+//const advancedPattern = /(?:\+?([\d|o|O]{1,3}))?[-. (]*([\d|o|O]+\w+([\d|o|O]+)|\w+){10,}/; 
+const advancedPattern = /([\d|o|O]+(\w+|\W+|\*)([\d|o|O]+)|\w+){10,}/; //check for tricks like 038tam88888
+const spacedPattern = /([\d|o|O]+\s*){7,11}/; //check for spaced numbers
 
-export function cleanPhoneNumber(text: string): string {
+const Patterns = [pattern, advancedPattern, spacedPattern];
+
+export function cleanPhoneNumber(text: string, patterns = Patterns): string {
   var output: string;
+  for (const pattern of patterns) {
+    if (pattern.test(text)) {
+      const matches = text.match(pattern);
+      const output = text.replace(matches[0].trim(), '{{phone_number}}');
+      return output
+    }
+  } 
+    return text
+  }
+  /*
   if (pattern.test(text) || advancedPattern.test(text)) {
     const matches = pattern.exec(text) || advancedPattern.exec(text);
     output = text.replace(matches[0].trim(), '{{phone_number}}');
@@ -11,4 +25,4 @@ export function cleanPhoneNumber(text: string): string {
   } else {
     return text;
   }
-}
+  */
