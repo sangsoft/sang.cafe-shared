e59738d97e4ebfc1e11b5778030d02ad18faa173
nquangtrung <nquangtrung@gmail.com>
2021-08-11 09:19:55 +0700

    Add more sample tests

--- src/__tests__/utils/content.cleanPhoneNumber.test.ts
+++ src/__tests__/utils/content.cleanPhoneNumber.test.ts
@@ -14,6 +14,36 @@ Sang cửa hàng ở 108 Nam Đồng, Đống Đa, Hà Nội.
 Ai có nhu cầu liên hệ số đt {{phone_number}}.
 `
         ],
+        [
+`
+Sang cửa hàng ở 108 Nam Đồng, Đống Đa, Hà Nội.
+Ai có nhu cầu liên hệ số đt O328888888.
+`,
+`
+Sang cửa hàng ở 108 Nam Đồng, Đống Đa, Hà Nội.
+Ai có nhu cầu liên hệ số đt {{phone_number}}.
+`
+        ],
+        [
+`
+Sang cửa hàng ở 108 Nam Đồng, Đống Đa, Hà Nội.
+Ai có nhu cầu liên hệ số đt O32tam888888.
+`,
+`
+Sang cửa hàng ở 108 Nam Đồng, Đống Đa, Hà Nội.
+Ai có nhu cầu liên hệ số đt {{phone_number}}.
+`
+        ],
+        [
+`
+Sang cửa hàng ở 108 Nam Đồng, Đống Đa, Hà Nội.
+Ai có nhu cầu liên hệ số đt 032tam888888.
+`,
+`
+Sang cửa hàng ở 108 Nam Đồng, Đống Đa, Hà Nội.
+Ai có nhu cầu liên hệ số đt {{phone_number}}.
+`
+        ],
       ])('cleanPhoneNumber(%p) should be %p', (text: string, expected: string) => {
         expect(cleanPhoneNumber(text)).toEqual(expected);
       })
