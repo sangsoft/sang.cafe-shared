af9b9fb50454cef64dbf89ba0975ce8105429eee
nquangtrung <nquangtrung@gmail.com>
2021-08-12 08:37:44 +0700

    Add more tests

--- src/__tests__/utils/content.cleanPhoneNumber.test.ts
+++ src/__tests__/utils/content.cleanPhoneNumber.test.ts
@@ -124,6 +124,16 @@ Ai có nhu cầu liên hệ số đt {{phone_number}}.
           Ai có nhu cầu liên hệ số đt {{phone_number}}.
           `
         ],
+        [
+          `
+          Sang cửa hàng ở 108/265/13/34 Nam Đồng, Đống Đa, Hà Nội.
+          Ai có nhu cầu liên hệ số đt 032.666.7777 hoặc 032.999.2222.
+          `,
+          `
+          Sang cửa hàng ở 108/265/13/34 Nam Đồng, Đống Đa, Hà Nội.
+          Ai có nhu cầu liên hệ số đt {{phone_number}} hoặc {{phone_number}}.
+          `
+        ],
       ])('cleanPhoneNumber(%p) should be %p', (text: string, expected: string) => {
         expect(cleanPhoneNumber(text)).toEqual(expected);
       })
