a3e851033e1b2d46d15d0e5650c1d62e28021676
Tung Dam <damtung2000@gmail.com>
2021-08-12 11:52:10 +0900

    cleanPhoneNumber will now apply all tests to the input strings

--- src/helpers/content.ts
+++ src/helpers/content.ts
@@ -1,5 +1,7 @@
-const pattern = /(?:\+?([\d|o|O]{1,3}))?[-. (]*([\d|o|O]{3})[-. )]*([\d|o|O]{3,4})[-. ]*([\d|o|O]{4})/; //matches most phone numbers w/wo hyphen, dots, spaces, country code
-const spacedPattern = /([\d|o|O]+\s*){7,11}/; //check for spaced numbers
+import { match } from "assert";
+
+const pattern = /(?:\+?([\d|o|O]{1,3}))?[-. (]*([\d|o|O]{3})[-. )]*([\d|o|O]{3,4})[-. ]*([\d|o|O]{4})/g; //matches most phone numbers w/wo hyphen, dots, spaces, country code
+const spacedPattern = /([\d|o|O]+\s*){7,11}/g; //check for spaced numbers
 const substitutionPattern = /([\d|o|O]+([\w|\*])+[\d|o|O]+[\w|\*]*){2,}/g; //check for tricks like 038tam88888, 038*88888, 03888888bay
 const substitutionKey = /\s*([\w|\*])+\s*[\=\:]\s*\d+/g; //check for substitution keys like x = 3, *:4
 const Patterns = [pattern, substitutionPattern, spacedPattern];
@@ -13,17 +15,17 @@ function removeSubstitutionKey(text: string): string {
 }
 
 export function cleanPhoneNumber(text: string, patterns = Patterns): string {
-  const textSubstitutionKeyRemoved = removeSubstitutionKey(text);
+  var output = removeSubstitutionKey(text);
   for (const pattern of patterns) {
-    if (pattern.test(textSubstitutionKeyRemoved)) {
-      const matches = textSubstitutionKeyRemoved.match(pattern);
-      return textSubstitutionKeyRemoved.replace(matches[0].trim(), '{{phone_number}}');
+    if (pattern.test(output)) {
+      const matches = output.match(pattern);
+      for (const match of matches) {
+        output = output.replace(match.trim(), '{{phone_number}}');
+      }
     }
   }
-
-  return text;
+  return output;
 }
-
 export function cleanAddress(text: string): string {
   return text;
 }
