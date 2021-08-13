fcbdf39f6b53d0b02beda8380babd7c0f4e5cbe2
Tung Dam <damtung2000@gmail.com>
2021-08-14 01:09:24 +0900

    Added streetName regex patterns

--- src/helpers/content.ts
+++ src/helpers/content.ts
@@ -1,8 +1,9 @@
-const pattern = /\b(?:\+?([\d|o|O]{1,3}))?[-. (]*(?:[\d|o|O]{3})[-. )]*(?:[\d|o|O]{3,4})[-. ]*(?:[\d|o|O]{4})/g; //matches most phone numbers w/wo hyphen, dots, spaces, country code
-const spacedPattern = /\b(?:[\d|o|O]{3,4}[\s\.]*){3,11}\b/g; //check for spaced numbers
-const substitutionPattern = /\b(?:[\d|o|O]+(?:[\w|\*])+[\d|o|O]+[\w|\*]*){2,}\b/g; //check for tricks like 038tam88888, 038*88888, 03888888bay
+//clean Phone Numbers
+const phonePattern = /\b(?:\+?([\d|o|O]{1,3}))?[-. (]*(?:[\d|o|O]{3})[-. )]*(?:[\d|o|O]{3,4})[-. ]*(?:[\d|o|O]{4})/g; //matches most phone numbers w/wo hyphen, dots, spaces, country code
+const spacedPhonePattern = /\b(?:[\d|o|O]{3,4}[\s\.]*){3,11}\b/g; //check for spaced numbers
+const substitutionPhonePattern = /\b(?:[\d|o|O]+(?:[\w|\*])+[\d|o|O]+[\w|\*]*){2,}\b/g; //check for tricks like 038tam88888, 038*88888, 03888888bay
 const substitutionKey = /\s*(?:[\w|\*])\s*[\=]\s*\d{0,2}\b/g; //check for substitution keys like x = 3, *:4
-const Patterns = [pattern, substitutionPattern, spacedPattern];
+const Patterns = [phonePattern, substitutionPhonePattern, spacedPhonePattern];
 
 function removeSubstitutionKey(text: string): string {
   if (substitutionKey.test(text)) {
@@ -22,6 +23,13 @@ export function cleanPhoneNumber(text: string, patterns = Patterns): string {
   return output;
 }
 
-export function cleanAddress(text: string): string {
+//Clean Address
+const streetName = /(?:phố|đường)\s(?:\s?[A-Z]\pL+)+/
+const streetNameWithNumber = /(?:số|ngõ|ngách)? \d{1,3} (?:phố|đường)?\s(?:\s?[A-Z]\pL+)+/
+
+const streetPatterns = [streetName, streetNameWithNumber]
+
+export function cleanAddress(text: string, patterns = streetPatterns): string {
+  cleanPhoneNumber(text, patterns)
   return text;
 }
