0d7cbcd01bf3754e79fa6fcb9c47cbaf42917c69
Tung Dam <damtung2000@gmail.com>
2021-08-13 11:44:50 +0900

    Added world boundaries to regex patterns so that the matched numbers are contained

--- src/helpers/content.ts
+++ src/helpers/content.ts
@@ -1,7 +1,7 @@
-const pattern = /(?:\+?([\d|o|O]{1,3}))?[-. (]*(?:[\d|o|O]{3})[-. )]*(?:[\d|o|O]{3,4})[-. ]*(?:[\d|o|O]{4})/g; //matches most phone numbers w/wo hyphen, dots, spaces, country code
-const spacedPattern = /(?:[\d|o|O]+\s*){7,11}/g; //check for spaced numbers
-const substitutionPattern = /(?:[\d|o|O]+(?:[\w|\*])+[\d|o|O]+[\w|\*]*){2,}/g; //check for tricks like 038tam88888, 038*88888, 03888888bay
-const substitutionKey = /\s*(?:[\w|\*])+\s*[\=\:]\s*\d+/g; //check for substitution keys like x = 3, *:4
+const pattern = /\b(?:\+?([\d|o|O]{1,3}))?[-. (]*(?:[\d|o|O]{3})[-. )]*(?:[\d|o|O]{3,4})[-. ]*(?:[\d|o|O]{4})\b/g; //matches most phone numbers w/wo hyphen, dots, spaces, country code
+const spacedPattern = /\b(?:[\d|o|O]{3,4}[\s\.]*){3,11}\b/g; //check for spaced numbers
+const substitutionPattern = /\b(?:[\d|o|O]+(?:[\w|\*])+[\d|o|O]+[\w|\*]*){2,}\b/g; //check for tricks like 038tam88888, 038*88888, 03888888bay
+const substitutionKey = /\s*(?:[\w|\*])\s*[\=]\s*\d{0,2}\b/g; //check for substitution keys like x = 3, *:4
 const Patterns = [pattern, substitutionPattern, spacedPattern];
 
 function removeSubstitutionKey(text: string): string {
