7c3ac8ca91fb784acea5c4df184d9b904f681c1f
Tung Dam <damtung2000@gmail.com>
2021-08-15 01:43:33 +0900

    Added district exclusion filter to address regex patterns

--- src/helpers/content.ts
+++ src/helpers/content.ts
@@ -1,9 +1,11 @@
+import { SUPPORTED_DISTRICTS, pL } from "../constants";
+
 //clean Phone Numbers
-const phonePattern = /\b(?:\+?([\d|o|O]{1,3}))?[-. (]*(?:[\d|o|O]{3})[-. )]*(?:[\d|o|O]{3,4})[-. ]*(?:[\d|o|O]{4})/g; //matches most phone numbers w/wo hyphen, dots, spaces, country code
+const generalPhonePattern = /\b(?:\+?([\d|o|O]{1,3}))?[-. (]*(?:[\d|o|O]{3})[-. )]*(?:[\d|o|O]{3,4})[-. ]*(?:[\d|o|O]{4})/g; //matches most phone numbers w/wo hyphen, dots, spaces, country code
 const spacedPhonePattern = /\b(?:[\d|o|O]{3,4}[\s\.]*){3,11}\b/g; //check for spaced numbers
 const substitutionPhonePattern = /\b(?:[\d|o|O]+(?:[\w|\*])+[\d|o|O]+[\w|\*]*){2,}\b/g; //check for tricks like 038tam88888, 038*88888, 03888888bay
 const substitutionKey = /\s*(?:[\w|\*])\s*[\=]\s*\d{0,2}\b/g; //check for substitution keys like x = 3, *:4
-const Patterns = [phonePattern, substitutionPhonePattern, spacedPhonePattern];
+const phonePatterns = [generalPhonePattern, substitutionPhonePattern, spacedPhonePattern];
 
 function removeSubstitutionKey(text: string): string {
   if (substitutionKey.test(text)) {
@@ -17,22 +19,24 @@ function matchPatternsAndReplace(text: string, patterns: RegExp[], placeholder: 
   const output = patterns.reduce((result, pattern) => {
     const matches = result.match(pattern);
     return (matches || []).reduce((result, match) => {
-      return result.replace(match.trim(), placeholder)
+      const formattedMatch = match.trim().replace(/,$/,'');
+      return result.replace(formattedMatch, placeholder)
     }, result);
   }, text);
   return output;
 }
 
-export function cleanPhoneNumber(text: string, patterns = Patterns): string {
+export function cleanPhoneNumber(text: string, patterns = phonePatterns): string {
   return matchPatternsAndReplace(removeSubstitutionKey(text), patterns, '{{phone_number}}');
 }
 
 //Clean Address
-const pL ='a-zA-ZÁáÀàẢảÃãẠạĂăẮắẰằẲẳẴẵẶặÂâẤấẦầẨẩẪẫẬậĐđÉéÈèẺẻẼẽẸẹÊêẾếỀềỂểỄễỆệÍíÌìỈỉĨĩỊịÓóÒòỎỏÕõỌọÔôỐốỒồỔổỖỗỘộƠơỚớỜờỞởỠỡỢợÚúÙùỦủŨũỤụƯưỨứỪừỬửỮữỰựÝýỲỳỶỷỸỹỴỵ';
-const streetName = new RegExp(`(?:phố|đường)\\s(?:\\s?[A-Z][${pL}]+)+`,'g');
-const streetNameWithNumber = new RegExp(`(?:số|ngõ|ngách)?\\s?\\d{1,3}(?:\\/\\d{1,3})?\\s(?:phố|đường)?s?(?:\\s?[A-ZĐ][${pL}]+)+`, 'g');
+const districtNames = Object.keys(SUPPORTED_DISTRICTS).reduce((result, city) => [...result, ...SUPPORTED_DISTRICTS[city]], []).join('|') //Quan 1|Quan 2|...
+const streetNamePattern = new RegExp(`(?:phố|đường)\\s(?:\\s?[A-Z][${pL}]+)+`,'g');
+const streetNameWithNumberPattern = new RegExp(`(?:số|ngõ|ngách)?\\s?\\d{1,3}(?:\\/\\d{1,3})?\\s(?:phố|đường)?s?(?:(?!${districtNames}|[Qq]uận|[Hh]uyện)\\s?[A-ZĐ][${pL}]+,?\\s?)+`, 'g');
 
-const streetPatterns = [streetName, streetNameWithNumber];
+
+const streetPatterns = [streetNamePattern, streetNameWithNumberPattern];
 
 export function cleanAddress(text: string, patterns = streetPatterns): string {
   return matchPatternsAndReplace(text, patterns, '{{address}}');
