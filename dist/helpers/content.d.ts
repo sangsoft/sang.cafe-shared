73d31ecccd4d394fba77382611ab7135995e6b4c
nquangtrung <nquangtrung@gmail.com>
2021-08-17 14:29:02 +0700

    Build

--- dist/helpers/content.d.ts
+++ dist/helpers/content.d.ts
@@ -1,2 +1,3 @@
+export declare function matchWithPatterns(text: string, patterns: RegExp[]): string[];
 export declare function cleanPhoneNumber(text: string, patterns?: RegExp[]): string;
 export declare function cleanAddress(text: string, patterns?: RegExp[]): string;
--- dist/helpers/content.js
+++ dist/helpers/content.js
@@ -1,12 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const constants_1 = require("../constants");
-//clean Phone Numbers
-const generalPhonePattern = /\b(?:\+?([\d|o|O]{1,3}))?[-. (]*(?:[\d|o|O]{3})[-. )]*(?:[\d|o|O]{3,4})[-. ]*(?:[\d|o|O]{4})/g; //matches most phone numbers w/wo hyphen, dots, spaces, country code
-const spacedPhonePattern = /\b(?:[\d|o|O]{3,4}[\s\.]*){3,11}\b/g; //check for spaced numbers
-const substitutionPhonePattern = /\b(?:[\d|o|O]+(?:[\w|\*])+[\d|o|O]+[\w|\*]*){2,}\b/g; //check for tricks like 038tam88888, 038*88888, 03888888bay
-const substitutionKey = /\s*(?:[\w|\*])\s*[\=]\s*\d{0,2}\b/g; //check for substitution keys like x = 3, *:4
-const phonePatterns = [generalPhonePattern, substitutionPhonePattern, spacedPhonePattern];
 function removeSubstitutionKey(text) {
     if (substitutionKey.test(text)) {
         const matches = text.match(substitutionKey);
@@ -15,18 +9,28 @@ function removeSubstitutionKey(text) {
     else
         return text;
 }
-function matchPatternsAndReplace(text, patterns, placeholder) {
-    const output = patterns.reduce((result, pattern) => {
-        const matches = result.match(pattern);
-        return (matches || []).reduce((result, match) => {
-            const formattedMatch = match.trim().replace(/,$/, '');
-            return result.replace(formattedMatch, placeholder);
-        }, result);
-    }, text);
-    return output;
+function matchWithPatterns(text, patterns) {
+    return patterns.reduce((result, pattern) => {
+        const matches = text.match(pattern);
+        return result.concat(matches);
+    }, []).filter(_ => !!_);
 }
+exports.matchWithPatterns = matchWithPatterns;
+function matchWithPatternsAndReplace(text, patterns, placeholder) {
+    const matches = matchWithPatterns(text, patterns);
+    return (matches || []).reduce((result, match) => {
+        const formattedMatch = match.trim().replace(/,$/, '');
+        return result.replace(formattedMatch, placeholder);
+    }, text);
+}
+//clean Phone Numbers
+const generalPhonePattern = /\b(?:\+?([\d|o|O]{1,3}))?[-. (]*(?:[\d|o|O]{3})[-. )]*(?:[\d|o|O]{3,4})[-. ]*(?:[\d|o|O]{4})/g; //matches most phone numbers w/wo hyphen, dots, spaces, country code
+const spacedPhonePattern = /\b(?:[\d|o|O]{3,4}[\s\.]*){3,11}\b/g; //check for spaced numbers
+const substitutionPhonePattern = /\b(?:[\d|o|O]+(?:[\w|\*])+[\d|o|O]+[\w|\*]*){2,}\b/g; //check for tricks like 038tam88888, 038*88888, 03888888bay
+const substitutionKey = /\s*(?:[\w|\*])\s*[\=]\s*\d{0,2}\b/g; //check for substitution keys like x = 3, *:4
+const phonePatterns = [generalPhonePattern, substitutionPhonePattern, spacedPhonePattern];
 function cleanPhoneNumber(text, patterns = phonePatterns) {
-    return matchPatternsAndReplace(removeSubstitutionKey(text), patterns, '{{phone_number}}');
+    return matchWithPatternsAndReplace(removeSubstitutionKey(text), patterns, '{{phone_number}}');
 }
 exports.cleanPhoneNumber = cleanPhoneNumber;
 //Clean Address
@@ -35,7 +39,7 @@ const streetNamePattern = new RegExp(`(?:phố|đường)\\s(?:\\s?[A-Z][${const
 const streetNameWithNumberPattern = new RegExp(`(?:số|ngõ|ngách)?\\s?\\d{1,3}(?:\\/\\d{1,3})?\\s(?:phố|đường)?s?(?:(?!${districtNames}|[Qq]uận|[Hh]uyện)\\s?[A-ZĐ][${constants_1.pL}]+,?\\s?)+`, 'g');
 const streetPatterns = [streetNamePattern, streetNameWithNumberPattern];
 function cleanAddress(text, patterns = streetPatterns) {
-    return matchPatternsAndReplace(text, patterns, '{{address}}');
+    return matchWithPatternsAndReplace(text, patterns, '{{address}}');
 }
 exports.cleanAddress = cleanAddress;
 //# sourceMappingURL=content.js.map
