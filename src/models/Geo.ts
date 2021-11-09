37b8086c7757a12496da8a4a0a7f593e11c456a4
nquangtrung <nquangtrung@gmail.com>
2021-11-09 14:04:06 +0700

    Add geo models

---
+++ src/models/Geo.ts
@@ -0,0 +1,21 @@
+export interface Coordinate {
+  lat: number;
+  lng: number;
+}
+
+export interface ILocationInput {
+  name: string;
+  city: string;
+  district: string;
+  coor: Coordinate;
+  size: number;
+  type: string;
+  note: string;
+  geohash: string;
+}
+
+export interface NearByResult {
+  location: ILocationInput;
+  distance: number;
+  distanceManhattan: number;
+}
