bfff0575b9972db7956cb2d144aab0b0f23e1de6
nquangtrung <nquangtrung@gmail.com>
2022-06-22 16:31:13 +0700

    Fix default data

--- src/models/Model.ts
+++ src/models/Model.ts
@@ -1,4 +1,9 @@
+import type { SerializedTimestamp } from '../helpers/times';
+import type * as admin from 'firebase-admin';
+
 export interface Model {
   uid?: string;
-  path: string;
+  path?: string;
+  updatedAt?: SerializedTimestamp | admin.firestore.Timestamp | Date | string;
+  createdAt?: SerializedTimestamp | admin.firestore.Timestamp | Date | string;
 }
