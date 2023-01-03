ebb0cee72a248ba3129c5a07717f74931f1fdbef
nquangtrung <nquangtrung@gmail.com>
2023-01-03 15:26:47 +0700

    Add cases

--- src/__tests__/utils/extractThuDucDistrict.test.ts
+++ src/__tests__/utils/extractThuDucDistrict.test.ts
@@ -5,6 +5,11 @@ describe('utils', () => {
   describe('extractThuDucDistrict', () => {
     it.each([
       ['2b Đường Lê Văn Miến, Thảo Điền, Quận 2, Hồ Chí Minh', 'Quận 2'],
+      ['2b Đường Lê Văn Miến, Thảo Điền, Q 2, Hồ Chí Minh', 'Quận 2'],
+      ['2b Đường Lê Văn Miến, Thảo Điền, Q.2, Hồ Chí Minh', 'Quận 2'],
+      ['2b Đường Lê Văn Miến, Thảo Điền, Quan 2, Hồ Chí Minh', 'Quận 2'],
+      ['2b Đường Lê Văn Miến, Thảo Điền, QuAn 2, Hồ Chí Minh', 'Quận 2'],
+      ['2b Đường Lê Văn Miến, Thảo Điền, quan 2, Hồ Chí Minh', 'Quận 2'],
     ])('normalizePhoneNumberNationalNoThrow(%p) should be %p', (input: string, expected: 'Thủ Đức' | 'Quận 9' | 'Quận 2') => {
       expect(extractThuDucDistrict(input)).toEqual(expected);
     });
